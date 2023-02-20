29c7fc206a184d4d269950aec02dade1b1439ef8
victorcngo <hello@victorcango.fr>
2023-02-20 23:16:32 +0100

    feat(pages): homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,25 @@
 <script setup>
-// const query = groq`*[_type == "film"]`
-// const sanity = useSanity()
-// const { data } = await useAsyncData('film', () => sanity.fetch(query))
-// const films = data._rawValue
+const query = groq`*[_type == "home"][0]
+  {
+    baseline,
+    featuredProjects[] -> {
+      title,
+      slug,
+      releaseDate,
+      mainImage,
+      mainVideo,
+      categories[] -> {
+        title,
+        slug
+      }
+    }
+  }
+`
+const sanity = useSanity()
+const { data } = await useAsyncData('home', () =>
+  sanity.fetch(query)
+)
+const home = data._rawValue
 </script>
 
 <template>
@@ -11,7 +28,28 @@
       <Title>1024</Title>
       <Meta name="description" content="1024 architecture website" />
     </Head>
+    <section class="hero">
+      <GridContainer>
+        <h1 class="title">
+          {{ home.baseline }}
+        </h1>
+      </GridContainer>
+    </section>
+    <FeaturedProjects :projects="home.featuredProjects" />
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.index {
+  .hero {
+    height: 100vh;
+    padding-top: 26rem;
+
+    .title {
+      grid-column: 2 / span 8;
+      font-size: $main-text-size;
+      font-weight: $extra-light;
+    }
+  }
+}
+</style>
