05ed81c883c30e825719507dd7d347452c67e268
victorcngo <hello@victorcango.fr>
2023-02-20 23:17:46 +0100

    feat(components): create utils components

---
+++ components/utils/GoBackButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button class="GoBackButton" @click="goBack()">Go back</button>
+</template>
+
+<script setup>
+const router = useRouter()
+
+function goBack() {
+  router.go(-1)
+}
+</script>
+
+<style lang="scss">
+.GoBackButton {
+  background-color: $white;
+  color: $black;
+  padding: 1rem;
+  cursor: pointer;
+}
+</style>
---
+++ components/utils/GridContainer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="GridContainer">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+.GridContainer {
+  @include grid(12, 1fr, 1, 0);
+  width: 100%;
+  padding: 0 2rem;
+}
+</style>
