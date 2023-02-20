4e89baaa077bf76164c7da3f1c2d702a6d167154
victorcngo <hello@victorcango.fr>
2023-02-20 23:18:02 +0100

    feat(components/nav): create SiteHeader

---
+++ components/nav/SiteHeader.vue
@@ -0,0 +1,37 @@
+<script setup>
+const isNavActive = useState('isNavActive')
+
+function toggleNav() {
+  console.log('Toggle nav')
+  isNavActive.value = !isNavActive.value
+  console.log(isNavActive.value)
+}
+</script>
+
+<template>
+  <header class="SiteHeader">
+    <SiteLogo />
+    <button @click="toggleNav()">Toggle Nav</button>
+  </header>
+</template>
+
+<style lang="scss">
+.SiteHeader {
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 10rem;
+  padding: 2rem;
+  width: 100%;
+  z-index: 30;
+
+  .SiteLogo {
+    svg {
+      width: 9rem;
+    }
+  }
+}
+</style>
