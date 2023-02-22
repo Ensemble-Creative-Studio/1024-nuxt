f47136db045940dd8849cb804e03c272d4fed7f6
victorcngo <hello@victorcango.fr>
2023-02-22 11:48:05 +0100

    feat(components): create utils components

---
+++ components/utils/BottomAnchors.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="BottomAnchors">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+.BottomAnchors {
+  height: 5rem;
+  padding: 1rem 1.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  bottom: 0;
+  z-index: 10;
+  background-color: $black;
+  font-size: 1.8rem;
+  color: $medium-grey;
+  width: 100%;
+  border-top: 0.1rem solid $dark-grey;
+
+  .BottomAnchors__list {
+    display: flex;
+    align-items: center;
+
+    .BottomAnchors__item {
+      &:not(:first-child) {
+        margin-left: 2rem;
+      }
+    }
+  }
+}
+</style>
---
+++ components/utils/VideoPlayer.vue
@@ -0,0 +1,35 @@
+<script setup>
+import MouseManager from '@/utils/MouseManager'
+import KeyboardManager from '@/utils/KeyboardManager'
+import { clamp, normalize } from '@/utils/math'
+import { contain } from '@/utils/geometry'
+import { parseTime } from '@/utils/string'
+import { closest } from '@/utils/dom'
+
+const props = defineProps({
+  id: String,
+})
+</script>
+
+<template>
+  <div class="VideoPlayer">
+    <img src="" alt="" />
+    <video src=""></video>
+    <div class="controls">Controls for video {{ id }}</div>
+  </div>
+</template>
+
+<style lang="scss">
+.VideoPlayer {
+  border: 0.1rem solid $white;
+  position: relative;
+  aspect-ratio: 110 / 62;
+
+  .controls {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+}
+</style>
