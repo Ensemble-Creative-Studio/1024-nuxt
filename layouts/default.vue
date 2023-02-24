c585a5c144eb96ed8ac9c61d7d3ea30371032578
victorcngo <hello@victorcango.fr>
2023-02-24 01:05:23 +0100

    feat(layouts): add NavOverlay

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,13 @@
 <script setup>
 const isNavActive = useState('isNavActive', () => false)
+
+function closeMenu() {
+  isNavActive.value = false
+}
+
 const route = useRoute()
 
-const query = groq`*[_type == "navMenu"][0]
-  {
-    ...,
-  }
-`
+const query = groq`*[_type == "navMenu"][0]`
 const sanity = useSanity()
 const { data } = await useAsyncData('navMenu', () => sanity.fetch(query))
 const navMenu = data._rawValue
@@ -24,6 +25,7 @@ watch(
   <div class="layout">
     <SiteHeader />
     <NavMenu :isNavActive="isNavActive" :navMenu="navMenu" />
+    <NavOverlay v-if="isNavActive" @mouseenter.native="closeMenu()" />
     <slot />
   </div>
 </template>
