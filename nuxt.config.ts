73c0b2b9fdc289212ece3981d952abe12711e8ab
dindonstudio <78362187+dindonstudio@users.noreply.github.com>
2024-03-04 15:44:21 +0100

    Revert "test: remove site map"
    This reverts commit fa8cd730aeb0e28af8bdca46f1ee05856d2f301b.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,7 +42,9 @@ const config: NuxtConfig & { sanity: typeof sanityConfig } = {
     },
   },
   generate: { fallback: '404.html' },
-
+  sitemap: {
+    
+  },
   site: {
     url: 'https://www.1024architecture.net/',
   },
