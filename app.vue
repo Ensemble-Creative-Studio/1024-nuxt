c4307df61a7541dccb96cd097723fd5c87cb9487
victorcngo <hello@victorcango.fr>
2024-05-17 01:41:42 +0200

    fix: registerPlugin

--- app.vue
+++ app.vue
@@ -2,6 +2,8 @@
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { ScrollToPlugin } from 'gsap/ScrollToPlugin'
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
 </script>
 
 <template>
